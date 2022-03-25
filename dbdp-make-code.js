c30aa679855d57dd0b57dc25a58dfccd477fa6f5
DESKTOP-6V8LD4B\tarek <hamidtarek3@gmail.com>
2022-03-25 07:19:08 -0400

    Adding static elements to MC webpage

---
+++ dbdp-make-code.js
@@ -0,0 +1,19 @@
+/* 
+    JavaScript for MakeCode application of DBDP. 
+*/
+
+// Vars
+var acc = document.getElementsByClassName("accordion");
+
+// Accordian side panel
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+}
