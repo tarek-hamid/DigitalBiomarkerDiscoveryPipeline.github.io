da608cc1d64fd89664964f0585b5a2981f9521d5
DESKTOP-6V8LD4B\tarek <hamidtarek3@gmail.com>
2022-05-03 01:29:45 -0400

    Adding demo code

--- dbdp-make-code.js
+++ dbdp-make-code.js
@@ -20,56 +20,56 @@ for (let i = 0; i < acc.length; i++) {
 }
 
 // Creates chart
-const ctx = document.getElementById('chart').getContext('2d');
-
 drawChart();
 
 async function drawChart() {
-  const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+  const datapoints = await getData();
+  const data = {
+    labels: datapoints.labels,
+    datasets: [{
+      label: 'Glucose',
+      data: datapoints.glucoseData,
+      backgroundColor: 'rgba(160,32,240)',
+      borderWidth: 1
+  }]};
+  
+  const config = {
+    type: 'line',
+    data,
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+      scales: {
+        y: {
+          beginAtZero: true
         }
+      }
     }
-  });
+  };
+
+  const myChart = new Chart(
+    document.getElementById('chart').getContext('2d'),
+    config
+  );
+
 }
 
 // Parses CSV
 async function getData() {
+  const labels = [];
+  const glucoseData = [];
   const url = '/output.csv'
   const response = await fetch(url)
   const tableData = await response.text();
-  console.log(tableData);
 
   const table = tableData.split('\n');
-  console.log(table)
+  table.forEach(row => {
+    const column = row.split(',')
+    const glucoseLevel = column[2];
+    const index = column[0];
+    labels.push(index);
+    glucoseData.push(glucoseLevel);
+  })
+
+  return { labels, glucoseData }
 }
 
 
